5874dc1a1c10ec4ac3470e5132e1daebdcab4b93
Rahul Churiwal <rahulchuriwal7@gmail.com>
2024-09-04 22:26:54 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyD7yR5UBY4UJjrDXoNFI_1X9k3qxJlFTi0" 
+API_KEY = <API_KEY>
 genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
